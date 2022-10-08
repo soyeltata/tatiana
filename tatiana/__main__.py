6d9418e8b0f43df06936e5afbf1988002d0cfad1
Alonzo Ximénez Qui <alonzo.ximenez.qui@gmail.com>
2022-10-08 19:25:42 +0200

    solucionado conflicto de rutas al importar los assets

--- tatiana/__main__.py
+++ tatiana/__main__.py
@@ -1,6 +1,7 @@
 from world import World
 from entity import Entity
+from os.path import abspath
 
-w = World(800,600,'running',Entity('base',(0,0),'assets/base.png'))
+w = World(800,600,'running',Entity('base',(0,0),abspath('assets/base.png')))
 w.set_color(0xff, 0xff, 0xff)
 w.run()
