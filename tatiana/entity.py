c3d16a4139a8c1f8aa74423b5a9948f53693ba70
Alonzo Ximénez Qui <alonzo.ximenez.qui@gmail.com>
2023-01-04 16:39:27 +0100

    escrita la documentación para los módulos `__init__.py`, `entity.py` y `typenames.py`

--- tatiana/entity.py
+++ tatiana/entity.py
@@ -1,10 +1,22 @@
+"""Módulo contenedor de la clase `Entity`, que tiene como propósito
+representar y modelar a una entidad cualquiera en el paradigma **ECS**.
+"""
 from typing import Any, Dict, Tuple, Optional
 from . import components
 
 
 class Entity:
+    """Clase encargada de representar y modelar una entidad cualquiera
+    en el paradigma **ECS**, teniendo a su vez como propiedades un **nombre**,
+    una colección de **componentes** y propiedades varias para información
+    adicional que deba de contener."""
     name: str
     components: Dict[Any, Any]
+    """Debido a la carencia de tipos de datos suficientes para modelar esta
+    situación, nos vemos obligados a describir el diccionario de componentes
+    como uno de tipos `Any` y `Any` para su clave y valor, sin embargo, lo que
+    se quiere representar es un diccionario que tomará una clase como clave y
+    una instancia de la misma como valor."""
     properties: Dict[str, Any]
 
     def __init__(
@@ -14,6 +26,12 @@ class Entity:
         sprite_path: Optional[str] = None,
         *ocomponents
     ) -> None:
+        """Constructor de la clase `Entity`, toma un nombre que será asignado
+        a la variable `name` del mismo objeto, una posición que se añadirá a los
+        componentes como un `TransformComponent`, una dirección (opcional) hacia
+        el sprite del dicho objeto que se agregará como un `SpriteComponent` y
+        soporta una colección de componentes como argumentos que se asignarán luego
+        a la propia entidad."""
         self.name = name
         self.components = {}
         self.properties = {}
@@ -26,25 +44,50 @@ class Entity:
         for component in ocomponents:
             self.add_component(component)
 
-    def add_component(self, component, *args):
+    def add_component(self, component: Any) -> None:
+        """Función encargada de agregar un componente a la entidad. Añade al
+        diccionario `components` de la clase el componente pasado como argumento
+        teniendo como clave su clase."""
         key = type(component)
         self.components[key] = component
 
-    def get_component(self, clazz):
-        return self.components[clazz]
+    def get_component(self, clazz: Any) -> Any:
+        """Función encargada de, a partir de una clase cualquiera pasada como
+        argumento, retornar el componente asociado a esta en el diccionario
+        `components` de la entidad."""
+        try:
+            return self.components[clazz]
+        except KeyError:
+            return None
 
-    def has_component(self, clazz):
+    def has_component(self, clazz: Any) -> bool:
+        """Función encargada de, a partir de una clase cualquiera pasada como
+        argumento, retornar un valor booleano que represente si existe un componente
+        asociado a esta."""
         return self.get_component(clazz) is not None
 
     def change_sprite(self, path: str) -> None:
+        """Función encargada de, a partir de una ruta determinada como una cadena
+        de texto, cambiar el sprite de la entidad por la imagen a la que apunta
+        la dicha ruta."""
         if self.has_component(components.SpriteComponent):
             self.components[components.SpriteComponent].change_sprite(1, path)
 
     def set_property(self, name: str, value: Any) -> None:
+        """Función encargada de asignar una propiedad a la entidad, sirviendo como
+        setter para el diccionario `properties` de la clase."""
         self.properties[name] = value
 
     def get_property(self, name: str) -> None:
+        """Función encargada de retornar una propiedad determinada de la entidad,
+        sirviendo como getter para el diccionario `properties` de la clase."""
         try:
             return self.properties[name]
-        except Exception:
+        except KeyError:
             return None
+
+    def has_property(self, name: str) -> bool:
+        """Función encargada de retornar un valor booleano que determine si existe
+        una propiedad asociada al nombre pasado como argumento en el diccionario
+        `properties` de la clase."""
+        return self.get_property(name) is not None
--- tatiana/typenames.py
+++ tatiana/typenames.py
@@ -1,2 +1,11 @@
+"""Módulo contenedor de los tipos de datos o `TypeVar`s (no clases)
+utilizados en el proyecto.
+"""
+
 from typing import TypeVar as __tv
 Self = __tv('Self')
+"""Al ser el tipo de dato `Self` exclusivo de Python v3.11
+y no funcionar del todo bien con mypy, se define en este
+módulo como un
+[`TypeVar`](https://docs.python.org/es/3/library/typing.html)
+cualquiera."""
